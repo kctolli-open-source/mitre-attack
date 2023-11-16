c50c370a50b8dfd9cf9f870dd4266640dc919da2
kctolli <developer@ktolliver.org>
2023-11-16 00:27:01 -0700

    execution

--- src/data/execution.ts
+++ src/data/execution.ts
@@ -2,8 +2,52 @@ import { stepsType } from './types';
 
 const steps: stepsType[] = [
     {
-        name: 'Acquire Access',
+        name: 'Cloud Administration Command',
     },
+    {
+        name: 'Command and Scripting Interpreter',
+        bullets: ['PowerShell', 'AppleScript', 'Windows Command Shell', 'Unix Shell', 'Visual Basic', 'Python', 'JavaScript', 'Network Device CLI', 'Cloud API'],
+    }, 
+    {
+        name: 'Container Administration Command',
+    },
+    {
+        name: 'Deploy Container',
+    },
+    {
+        name: 'Exploitation for Client Execution',
+    },
+    {
+        name: 'Inter-Process Communication',
+        bullets: ['Component Object Model', 'Dynamic Data Exchange', 'XPC Services'],
+    },
+    {
+        name: 'Native API',
+    },
+    {
+        name: 'Scheduled Task/Job',
+        bullets: ['At', 'Cron', 'Scheduled Task', 'Systemd Timers', 'Container Orchestration Job'],
+    },
+    {
+        name: 'Serverless Execution',
+    },
+    {
+        name: 'Shared Modules',
+    }, 
+    {
+        name: 'Software Deployment Tools',
+    },
+    {
+        name: 'System Services',
+        bullets: ['Launchctl', 'Service Execution']
+    },
+    {
+        name: 'User Execution',
+        bullets: ['Malicious Link', 'Malicious File', 'Malicious Image']
+    },
+    {
+        name: 'Windows Management Instrumentation',
+    }
 ];
 
 export default steps;
