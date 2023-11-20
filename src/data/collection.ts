99cb4d5bd4d365bb6523ff25215e95c324f85764
kctolli <developer@ktolliver.org>
2023-11-20 01:15:25 -0700

    Deploy --- 11/20/2023 01:15:25 AM

--- src/data/collection.ts
+++ src/data/collection.ts
@@ -1,5 +1,21 @@
 const steps: string[] = [
-    'Acquire Access',
+    'Adversary in the Middle',
+    'Archive Collected Data',
+    'Audio Capture',
+    'Automated Collection',
+    'Browser Session Hijacking',
+    'Clipboard Data',
+    'Data from Cloud Storage',
+    'Data from Configuration Repository',
+    'Data from Information Repositories',
+    'Data from Local System',
+    'Data from Network Shared Drive',
+    'Data from Removable Media',
+    'Data Staged',
+    'Email Collection',
+    'Input Capture',
+    'Screen Capture',
+    'Video Capture',
 ];
 
 export default steps;
