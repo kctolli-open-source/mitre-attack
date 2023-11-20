203c3fe8dd9d3c4083f20e4ad21b226bdbc05ec3
kctolli <developer@ktolliver.org>
2023-11-20 01:20:37 -0700

    Deploy --- 11/20/2023 01:20:37 AM

--- src/data/commandcontrol.ts
+++ src/data/commandcontrol.ts
@@ -1,5 +1,21 @@
 const steps: string[] = [
-    'Acquire Access',
+    'Application Layer Protocol',
+    'Communication Through Removable Media',
+    'Content Injection',
+    'Data Encoding',
+    'Data Obfuscation',
+    'Dynamic Resolution',
+    'Encrypted Channel',
+    'Fallback Channels',
+    'Ingress Tool Transfer',
+    'Multi-Stage Channels',
+    'Non-Application Layer Protocol',
+    'Non-Standard Port',
+    'Protocol Tunneling',
+    'Proxy',
+    'Remote Access Software',
+    'Traffic Signaling',
+    'Web Service',
 ];
 
 export default steps;
