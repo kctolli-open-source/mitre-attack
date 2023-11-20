d4c6b9d920608ebaa3cb0d19d62184b9fcb02ab9
kctolli <developer@ktolliver.org>
2023-11-20 01:09:08 -0700

    Deploy --- 11/20/2023 01:09:08 AM

--- src/data/impact.ts
+++ src/data/impact.ts
@@ -1,5 +1,18 @@
 const steps: string[] = [
-    'Acquire Access',
+    'Account Access Removal',
+    'Data Destruction',
+    'Data Encrypted for Impact',
+    'Data Manipulation',
+    'Defacement',
+    'Disk Wipe',
+    'Endpoint Denial of Service',
+    'Financial Theft',
+    'Firmware Corruption',
+    'Inhibit System Recovery',
+    'Network Denial of Service',
+    'Resource Hijacking',
+    'Service Stop',
+    'System Shutdown / Reboot',
 ];
 
 export default steps;
