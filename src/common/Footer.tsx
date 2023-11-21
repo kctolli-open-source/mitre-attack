c9cd5bebcaa8fd49c382c1effb9b43a52138cd6d
kctolli <developer@ktolliver.org>
2023-11-20 23:50:04 -0700

    Deploy --- 11/20/2023 11:50:04 PM

--- src/common/Footer.tsx
+++ src/common/Footer.tsx
@@ -1,9 +1,9 @@
 export default function Footer() {
     return (
         <footer>
-            &copy; {(new Date()).getFullYear()} Kyle Tolliver - Security Attack
+            &copy; {(new Date()).getFullYear()} Kyle Tolliver - <a href="/">Security Attack</a>
             <br /> 
-            All rights reserved - Mitre ATT&CK
+            All rights reserved - <a href="https://attack.mitre.org">Mitre ATT&CK</a>
         </footer>
     );    
 }
