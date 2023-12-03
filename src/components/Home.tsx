ac3e22d7f6dd67fee2dd0cf6d97cecceae336264
kctolli <developer@ktolliver.org>
2023-12-02 23:13:13 -0700

    Deploy --- 12/02/2023 11:13:13 PM

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -38,6 +38,8 @@ export default function Home(): JSX.Element {
                 <h3>Additional Info</h3>
                 <ul>
                     <li><BlankATag link="https://owasp.org/projects/" text="OWASP Projects" /> for examples to test against.</li>
+                    <li><BlankATag link="https://nvd.nist.gov/" text="National Vulnerability Database" /> contains a list of current vulnerabilities</li>
+                    <li><BlankATag link="https://www.cisa.gov/topics/cybersecurity-best-practices" text="Cybersecurity Best Practices" /> by Cybersecurity and Infrastructure Security Agency</li>
                 </ul>
             </article>
             <span key="Get Started" className="next-step">
